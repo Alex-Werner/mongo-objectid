08f60ad7f916811569da078bd5cca71160977e81
Alex Werner <alex@werner.fr>
2019-09-02 20:01:11 +0200

    test(objectid): adding more comprehensive tests

--- test/ObjectId.js
+++ test/ObjectId.js
@@ -2,14 +2,24 @@ const {expect} = require('chai');
 const ObjectId = require('../index');
 
 describe('ObjectId', () => {
-  it('should work', function () {
+  it('should create a new objectID', function () {
     const id = new ObjectId();
     expect(id.toString().length).to.equal(24)
 
+  });
+  it('should close', function () {
+    const id = new ObjectId();
     const id2 = new ObjectId(id);
     expect(id2.toString().length).to.equal(24);
     expect(id2).to.deep.equal(id);
-
-    expect(id2.isValid()).to.deep.equal(true)
+  });
+  it('should allow validation', ()=>{
+    const id = new ObjectId();
+    expect(id.isValid()).to.deep.equal(true);
+  })
+  it('should allow creation from hexString', function () {
+    const hex = '5d6d58a43dc43c05943ac983';
+    const id = new ObjectId(hex);
+    expect(id.toString()).to.equal(hex)
   });
 });
